e7a1f88c5cf5e9688d7b9d842fb56e6c887fd2ef
Aryan756-web <aryankaradkar2312@gmail.com>
2026-04-27 13:37:21 +0530

    fix API URLs for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "${import.meta.env.VITE_API_URL}", //  YOUR BACKEND
+  baseURL: "${import.meta.env.VITE_API_URL}/api", //  YOUR BACKEND
 });
 
 export default API;
